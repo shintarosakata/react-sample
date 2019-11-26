ddd0527aad4959932123167bf52f62c472fb6dad
shintaro.sakata <shintaro.sakata@leverages.jp>
2019-11-26 13:40:31 +0900

    add todolist

--- src/index.js
+++ src/index.js
@@ -2,9 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.scss';
 
-function App() {
+const { useState } = React;
+
+const App = () => {
+  // レンダリングされるtodos
+  const [todos, setTodos] = useState([]);
+
+  // formに入力中のtodo
+  const [tmpTodo, setTmpTodo] = useState("");
+
+  const addTodo = () => {
+    if (tmpTodo === "") { return alert("文字を入力してください") }
+    // Todosの最後にformのTodoを追加
+    setTodos([...todos, tmpTodo]);
+    // formの初期化
+    setTmpTodo("");
+  };
+
+  const deleteTodo = index =>
+    setTodos(todos.filter((_, todoIndex) => index !== todoIndex));
+
+
+  const liTodo = (todo, index) => {
+    return (
+      <li key={index}>
+        {todo}
+        <button onClick={() => deleteTodo(index)}>delete</button>
+      </li>
+    );
+  }
+
   return (
-    <h1>hello world</h1>
+    <>
+      <h1>Todo App</h1>
+      <div className="form">
+        <input type="text" name="todo" onChange={e => setTmpTodo(e.target.value)} value={tmpTodo} />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map(liTodo)}
+      </ul>
+    </>
   );
 }
 
